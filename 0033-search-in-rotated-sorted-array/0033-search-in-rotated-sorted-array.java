af665f37951509443ce3619d7bf13923299db010
Kashish <kashish27puri@gmail.com>
2026-07-27 16:29:51 +0530

    Time: 0 ms (100%), Space: 43.5 MB (92.46%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,17 +5,17 @@ class Solution {
         while(left <= right){
             int mid = left + (right - left)/2;
             if(nums[mid] == target) return mid;
-            if(nums[left] <= nums[mid]){
-                if(target >= nums[left] && target < nums[mid]){
+            else if(nums[left] <= nums[mid]){
+                if(target < nums[mid] && target >= nums[left]){
                     right = mid - 1;
                 }
                 else left = mid + 1;
             }
-            else {
-                if (target > nums[mid] && target <= nums[right])
+            else{
+                if(target > nums[mid] && target <= nums[right]){
                     left = mid + 1;
-                else
-                    right = mid - 1;
+                }
+                else right = mid - 1;
             }
         }
         return -1;
