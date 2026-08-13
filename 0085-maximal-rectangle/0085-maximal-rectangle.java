0d0de78e453b9d83c0e2b77bdbc9ac4c6c0e3990
Kashish <kashish27puri@gmail.com>
2026-08-13 17:38:04 +0530

    Time: 30 ms (29.68%), Space: 48.5 MB (84.25%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] heights = new int[cols];
+        int maxA = 0;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(matrix[i][j] == '1') heights[j]++;
+                else heights[j] = 0;
+            }
+            int area = largestRectangleArea(heights);
+            maxA = Math.max(maxA, area);
+        }
+        return maxA;
+    }
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Stack<Integer> st = new Stack<>();
+        int[] previous_smaller = new int[n];
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() &&
+                    heights[st.peek()] >= heights[i]) st.pop();
+            if (st.isEmpty()) {
+                previous_smaller[i] = -1;
+            } else {
+                previous_smaller[i] = st.peek();
+            }
+            st.push(i);
+        }
+        st.clear();
+
+        int[] next_smaller = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() &&
+                    heights[st.peek()] >= heights[i]) st.pop();
+            if (st.isEmpty()) {
+                next_smaller[i] = n;
+            } else {
+                next_smaller[i] = st.peek();
+            }
+            st.push(i);
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int width = next_smaller[i] - previous_smaller[i] - 1;
+            int area = heights[i] * width;
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+}
