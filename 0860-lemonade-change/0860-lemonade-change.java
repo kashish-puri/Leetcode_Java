a876cc24aafb0dc303c924e8768a77bfc49fca46
Kashish <kashish27puri@gmail.com>
2026-08-19 18:28:00 +0530

    Time: 2 ms (97.97%), Space: 72.5 MB (30.29%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -9,14 +9,16 @@ class Solution {
                     five--;
                 }
                 else return false;
-            }else{
-                if(ten > 0 && five > 0){
-                    ten--;
+            }
+            else{
+                if(five > 0 && ten > 0){
                     five--;
+                    ten--;
                 }
                 else if(five >= 3){
                     five -= 3;
-                }else return false;
+                }
+                else return false;
             }
         }
         return true;
