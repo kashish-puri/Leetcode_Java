2011d2cb5257e3756410e97d15d7e73404785efa
Kashish <kashish27puri@gmail.com>
2026-09-10 13:28:23 +0530

    Time: 135 ms (30.06%), Space: 45.8 MB (85.24%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
@@ -1,31 +1,33 @@
 class Solution {
-    int N=10000001;
-    int[] prime=new int[N];
+    int N = 1000001;
+    boolean[] prime = new boolean[N];
     public void sieve(){
-        Arrays.fill(prime, 1);
-        prime[0]=0;
-        prime[1]=0;
-        for(int i=2;i*i<N;i++){
-            if(prime[i]==1){
-                for(int j=i*i;j<N;j+=i){
-                    prime[j]=0;
+        Arrays.fill(prime,true);
+        prime[0] = false;
+        prime[1] = false;
+        for(int i = 2; i * i < N; i++){
+            if(prime[i] == true){
+                for(int j = i * i; j < N; j += i){
+                    prime[j] = false;
                 }
             }
         }
+
     }
     public int[] closestPrimes(int left, int right) {
         sieve();
-        int[] arr={-1,-1};
-        int prev=-1;
-        int diff=Integer.MAX_VALUE;
-        for(int i=left; i<=right; i++){
-            if(prime[i]==0) continue;
-            if(prev!=-1 && i-prev<diff){
-                diff=i-prev;
-                arr[0]=prev;
-                arr[1]=i;
+        int[] arr = {-1,-1};
+        int prev = -1, diff = Integer.MAX_VALUE;
+        for(int i = left; i <= right; i++){
+            if(prime[i]){
+                if(prev != -1 && i - prev < diff){
+                    diff = i - prev;
+                    arr[0] = prev;
+                    arr[1] = i;
+                }
+                prev = i;
             }
-            prev=i;
+            
         }
         return arr;
     }
